8b99a4860af5a0db2c1e2f5215632c3b281ed959
NileshJoshi <nileshjoshi1805@gmail.com>
2024-06-24 15:56:11 +0530

    Make the cartQuantity interactive

--- Amazon_Project/amazon.js
+++ Amazon_Project/amazon.js
@@ -1,7 +1,82 @@
-let cartNumber = 0;
-function cartAddition(){
-    cartNumber ++;
-    console.log(cartNumber);
-    document.querySelector('.cartNumber1').innerHTML = cartNumber;
-    return cartNumber;
-}
+let productHTML = '';
+products.forEach((product) => {
+    productHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image" src="${product.image}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+                <img class="product-rating-stars"
+                src="ratings/rating-${product.rating.stars * 10}.png">
+                <div class="product-rating-count link-primary">
+                ${product.rating.count}
+                </div>
+            </div>
+
+            <div class="product-price">
+                ₹${product.priceCents}
+            </div>
+
+            <div class="product-quantity-container">
+                <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+                <img src="checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
+                Add to Cart
+            </button>
+        </div>
+    `;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click',() => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+    });
+    if (matchingItem){
+        matchingItem.quantity += 1;
+    }else{
+        cart.push({productId: productId,
+                  quantity: 1
+        });
+    }
+
+    let cartQuantity = 0;
+    cart.forEach((number) =>{
+        cartQuantity += number.quantity;;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity);
+    console.log(cart);
+    });
+});
--- OtherMinorProjects/javascript.js
+++
@@ -1,6 +0,0 @@
-let name = ["nilesh","90","onion","sfefef","sefefef","effafaefa"];
-for (let i=0;i<=name.length;i++)
-    {
-        console.log(name[i]);
-    }
-console.log(name);
